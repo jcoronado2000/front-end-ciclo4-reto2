aaf9ef52581a0d050df97965e5b0dbe614de37da
Juanbonilla10 <coronadobonillajuancarlos904@gmail.com>
2021-12-06 15:05:10 -0500

    fullVersion

--- administracionUsuario.js
+++ administracionUsuario.js
@@ -2,8 +2,8 @@
  * EndPoint
  */
 
-//const endPointUser = "http://localhost:8080/api/user";
-const endPointUser = "http://150.230.88.187:8080/api/user"
+const endPointUser = "http://localhost:8080/api/user";
+//const endPointUser = "http://150.230.88.187:8080/api/user"
 
 let id;
 let id_creacion = 1;
@@ -99,7 +99,7 @@ function eliminarUser() {
     let identificacion = $("#exampleEliminarUser").val();
     $.ajax({
         method: "DELETE",
-        url: endPointUser + "/eliminarId/" + identificacion,
+        url: endPointUser + "/eliminar/" + identificacion,
         success: function (data) {
             console.log(data);
             swal("", `Registro eliminado con exito `, "success")
